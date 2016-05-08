031726e37c7a4bc65674279dadd396644997ac94
multivoxmuse <turner.micah@gmail.com>
2016-05-07 21:07:43 -0600

    a few working js functions and a Vue. I have learned that CORS requests wont work with voat so I will use a backend API call instead

--- voat-music/static/js/voat-music.js
+++ voat-music/static/js/voat-music.js
@@ -1,12 +1,27 @@
-var getyturl = {
-  yturl: 'https://www.youtube.com/embed/wyPfrbJKMpg'
-}
+$(document).ready(function(){
+  var VoatAPI = "https://voat.co/api/subversefrontpage?subverse=music"
 
-var ytiframe = new Vue({
-  el: '#ytembed',
-  data: getyturl
-})
+  var getyturl = {
+    yturl: 'https://www.youtube.com/embed/wyPfrbJKMpg'
+  }
 
+  var postvue = new Vue({
+    el: '#posts',
+    data: {
+      posts: {}
+    }
+  });
 
+  $.ajax({
+    url: VoatAPI,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) { console.log('hello!'); },
+    error: function() { console.log('boo!'); },
+    beforeSend: function(xhr){ 
+      xhr.setRequestHeader("Content-Type","application/json");
+      xhr.setRequestHeader("Accept","application/json");
+    }
+  });
 
-
+});
---
+++ voat-music/voat-rack.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# Simple backend for the voat music player
+require 'rack'
+app = Proc.new do |env|
+  [
+    '200', 
+    {'Content-Type' => 'text/html'}, 
+    ['A barebones rack app.']
+  ]
+end
+Rack::Handler::WEBrick.run app
